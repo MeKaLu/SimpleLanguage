e8ea70ccc926ac3341782ae199c11296414bebb8
Mehmet Kaan Uluç <80545079+MeKaLu@users.noreply.github.com>
2023-05-05 06:07:58 +0300

    single pin, func and arg working

--- simpleLanguage/simple_language.c
+++ simpleLanguage/simple_language.c
@@ -142,31 +142,91 @@ void simpleLangExecute(const char* code, const unsigned short code_size) {
 	char state = STATE_ZERO;
 
 	bool skip = false;
+	bool dont_append = false;
+	bool end_statement = false;
+	bool fresh = false;
 
 	while (i < code_size) {
 		c = code[i];
 		if (c == '\n') {
-			linec++;
-			word[0] = '\0';
-			word_len = 0;
 			if (skip) skip = false;
 			else if (!skip) {
-				if (state != STATE_ZERO) {
+				if ((state == STATE_ZERO || (state != STATE_ZERO && state != STATE_ARGUMENT)) && word_len > 0) {
 					// there should be ';' at the end of each statement but there was not
 					set_error("UnfinishedStatement");
 					goto force_error;
 				}
 			}
+			linec++;
+			word[0] = '\0';
+			word_len = 0;
 		} else if (c == '\r' || c == '\t') {
 		} else if (c == SPECIAL_COMMENT) {
 			skip = true;
 		} else if (!skip) {
 			// lines cannot start with special characters
-			if (c == SPECIAL_COMBINE || c == SPECIAL_CONDITION || c == SPECIAL_END) {
-				set_error("LinesCannotStartWithSpecialChars");
+			if (state == STATE_ZERO && word_len == 0 && (c == SPECIAL_COMBINE || c == SPECIAL_CONDITION || c == SPECIAL_END)) {
+				set_error("CannotStartWithSpecialChars");
 				goto force_error;
+			} else if ((c == SPECIAL_COMBINE || c == SPECIAL_CONDITION || c == SPECIAL_END)) {
+				if (c == SPECIAL_END && word_len > 0) {
+					// word has ended, more like the entire statement ended though
+					// so for that to happen, there has to be at least a function
+					if (state == STATE_FUNCTION) {
+						// it's single argument function
+						dont_append = true;
+						end_statement = true;
+
+						old_state = state;
+						state = STATE_ARGUMENT;
+						goto skip_to_object_append;
+					} else if (state == STATE_PIN) {
+						// no argument function
+						dont_append = true;
+						end_statement = true;
+						// fresh start
+						fresh = true;
+
+						old_state = state;
+						state = STATE_FUNCTION;
+						goto skip_to_object_append;
+					} else if (state == STATE_ARGUMENT) {
+						dont_append = true;
+						end_statement = true;
+
+						fresh = true;
+						goto skip_to_object_append;
+					}
+				} else if (c == SPECIAL_COMBINE) {
+					if (state == STATE_FUNCTION) {
+						// syntax rule error
+						set_error("InvalidUseOfCombine");
+						goto force_error;
+					}
+				} else if (c == SPECIAL_CONDITION) {
+					if (state == STATE_FUNCTION) {
+						// syntax rule error
+						set_error("InvalidUseOfCondition");
+						goto force_error;
+					}
+				}
+			} else if (c != ' ' && word_len > 0) {
 			} else if (c == ' ' && word_len > 0) {
-				// a word
+				if (state == STATE_PIN && old_state == STATE_ZERO) {
+					// there is going to be a function
+					old_state = state;
+					state = STATE_FUNCTION;
+				} else if (state == STATE_FUNCTION && old_state == STATE_PIN) {
+					// prevent double functions
+					old_state = state;
+					state = STATE_ARGUMENT;
+				} else if (state == STATE_PIN && old_state == STATE_ARGUMENT) {
+					// statement ended
+					old_state = STATE_ZERO;
+				}
+			skip_to_object_append:
+				bool reject_obj = false;
+				word[word_len] = '\0';
 
 				// +1 for null character at the end, strcpy will add it
 				word_len++;
@@ -178,13 +238,11 @@ void simpleLangExecute(const char* code, const unsigned short code_size) {
 				}
 
 #ifdef SIMPLE_LANGUAGE_ERROR_MESSAGE
-				printf("Line: %i | Word: %s\n", linec, word_copy);
+				printf("Line: %i | Word: '%s' | States: %c & %c\n", linec, (const char*)word_copy, old_state, state);
 #endif
 
-				bool reject_obj = false;
-
 				// fresh start
-				// that means there HAS to be a pin
+				// that means word HAS to be a pin
 				if (state == STATE_ZERO) {
 					state = STATE_PIN;
 				}
@@ -221,7 +279,7 @@ void simpleLangExecute(const char* code, const unsigned short code_size) {
 					}
 					// increment the id for the next obj
 					object_id++;
-
+					dont_append = true;
 
 #ifdef SIMPLE_LANGUAGE_ERROR_MESSAGE
 					printf("Id: %i | Type: %c\n", obj.id, obj.type);
@@ -231,12 +289,26 @@ void simpleLangExecute(const char* code, const unsigned short code_size) {
 					printf("^rejected\n");
 #endif
 					sfree(word_copy);
+				} 
+				if (end_statement) {
+					// fresh start
+					//if (old_state == STATE_FUNCTION) {
+					if (fresh) {
+						old_state = STATE_ZERO;
+						state = STATE_ZERO;
+						fresh = false;
+					} else {
+						old_state = state;
+						state = STATE_PIN;
+					}
+					end_statement = false;
 				}
-
 				word_len = 0;
 			}
-			word[word_len] = c;
-			word_len++;
+			if (!dont_append) {
+				word[word_len] = c;
+				word_len++;
+			} else dont_append = false;
 		}
 		i++;
 	}
@@ -251,9 +323,9 @@ void simpleLangExecute(const char* code, const unsigned short code_size) {
 
 force_error:
 #if SIMPLE_LANGUAGE_ERROR_MESSAGE
-	printf("-------------------\nERR:%s\n\tLINE: %i WORD: %s CHAR: %c\n\tLAST STATEs: %c & %c\n", error, linec, word, c, old_state, state);
+	printf("-------------------\nERR:%s\n\tLINE: %i WORD: '%s' CHAR: '%c' \n\tLAST STATEs: %c & %c\n", error, linec, word, c, old_state, state);
 #else
-	printf("-------------------\nERROR(message is disabled)\n\tLINE: %i WORD: %s CHAR: %c\n\tLAST STATEs: %c & %c\n", linec, word, c, old_state, state);
+	printf("-------------------\nERROR(message is disabled)\n\tLINE: %i WORD: '%s' CHAR: '%c' \n\tLAST STATEs: %c & %c\n", linec, word, c, old_state, state);
 #endif
 	objectListFree();
 	return;
