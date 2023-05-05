f96418f4d428d2575f9a25cb7d0e1feca143d3cf
Mehmet Kaan Uluç <80545079+MeKaLu@users.noreply.github.com>
2023-05-05 03:44:01 +0300

    blank state

--- simpleLanguage/simple_language.c
+++ simpleLanguage/simple_language.c
@@ -9,6 +9,7 @@
 	C -> combine	[C|n(P)|C|F|n(A)]
 	S -> condition	[P|S|n(B)|F|n(A)] or [C|n(P)|S|n(B)|F|n(A)]
 */
+#define STATE_ZERO						'0'
 #define STATE_PIN						'P'
 #define STATE_FUNCTION					'F'
 #define STATE_ARGUMENT					'A'
@@ -130,15 +131,15 @@ void simpleLangExecute(const char* code, const unsigned short code_size) {
 #if SIMPLE_LANGUAGE_ERROR_MESSAGE
 	char error[SIMPLE_LANGUAGE_ERROR_LEN];
 #endif
-	set_error("UNKOWN");
+	set_error("Unkown");
 
 	unsigned char word_i = 0;
 	char word[SIMPLE_LANGUAGE_MAX_WORD_LEN];
 	strfill(word, SIMPLE_LANGUAGE_MAX_WORD_LEN, '\0');
 	
-	char old_state = STATE_PIN;
-	char state = STATE_PIN;
-	bool end = false;
+	char old_state = STATE_ZERO;
+	char state = STATE_ZERO;
+
 	bool skip = false;
 
 	while (i < code_size) {
@@ -146,213 +147,22 @@ void simpleLangExecute(const char* code, const unsigned short code_size) {
 		if (c == '\n') {
 			linec++;
 			if (skip) skip = false;
-			else if (!skip && state == STATE_ARGUMENT) {
-				// there should be ';' at the end of each statement but there was not
-				set_error("UNFINISHEDSTATEMENT");
-				goto force_error;
+			else if (!skip) {
+				if (state == STATE_ZERO) {
+					linec++;
+					i++;
+					continue;
+				} else {
+					// there should be ';' at the end of each statement but there was not
+					set_error("UnfinishedStatement");
+					goto force_error;
+				}
 			}
 		} else if (c == '\r' || c == '\t') {
 		} else if (c == SPECIAL_COMMENT) {
 			skip = true;
 		} else {
-			bool dont_append = false;
-			if (!skip) {
-				// spaces are very important in this language
-				if (c == ' ') {
-					dont_append = true;
-				force_object:
-					// rejection can still happen even if the object is forced
-					bool reject = false;
-					word[word_i] = '\0';
-
-					// if there is a rejection it will be freed at the end,
-					// else it will be freed when object list is getting freed
-					void* word_copy = smalloc(sizeof(char) * word_i + 1);
-					if (!strcpy((char*)word_copy, word, word_i + 1)) {
-						// word was longer than word_i
-						goto force_error;
-					}
-
-					// create somewhat random word id, there should be no possible 
-					// same id created here but just in case it will check and act accordingly
-					short id = word_i + ((i != 0) ? i / 10 : 0);
-					while (objectListFind(id) != -1) id++;
-					SimpleLangObject obj = (SimpleLangObject){
-						.ptr = word_copy,
-						.size = word_i + 1,
-						.id = id,
-						.type = state,
-					};
-
-#if SIMPLE_LANGUAGE_ERROR_MESSAGE
-					printf("%s\n", (const char*)word_copy);
-#endif
-
-					if (state == STATE_PIN) {
-						// 'word_i > 0' hardcoded to avoid empty objects
-						// if 'word_i == 0' it will reject the object append
-						// otherwise pin will slide into the function and function will slide into argument
-						if (word_i > 0) {
-							if (old_state == STATE_COMBINE) {
-								// [C] is empty
-							} else {
-								old_state = state;
-								state = STATE_FUNCTION;
-							}
-						} else {
-							// hardcoded to avoid empty objects
-							if (word_i == 0) reject = true; // possible need of escape here, or else it may interfere
-							// multiple pins
-							// old state should be COMBINE
-							else if (old_state != STATE_COMBINE) {
-								set_error("UNKNOWNUSEOFPINS");
-								goto force_error;
-							} else goto force_error;
-						}
-					} else if (state == STATE_FUNCTION) {
-						// check if function exists
-						// hardcoded, [C|n(P)|F(empty)|A(F is here)|n(A)]
-						// sliding the object types avoided with this
-						if (word_i == 0) reject = true;
-						else {
-							old_state = state;
-							state = STATE_ARGUMENT;
-						}
-					} else if (state == STATE_ARGUMENT) {
-						if (old_state == STATE_FUNCTION) {
-							// check if param exists in the function
-							if (end) {
-								// add the argument then reset the state
-								end = false;
-								old_state = state;
-								state = STATE_PIN;
-							}
-						} else {
-							set_error("UNKNOWNUSEOFARGUMENTS");
-							goto force_error;
-						}
-					} else if (state == STATE_COMBINE) {
-						// Combine
-						old_state = state;
-						state = STATE_PIN;
-					} else if (state == STATE_CONDITION) {
-						// hardcoded to avoid empty objects
-						if (word_i == 0) reject = true; // possible need of escape here, or else it may interfere
-						else {
-							old_state = state;
-							state = STATE_CONDITION_ARGUMENT;
-						}
-					} else if (state == STATE_CONDITION_ARGUMENT) {
-						// hardcoded to avoid empty objects
-						if (word_i == 0) reject = true; // possible need of escape here, or else it may interfere
-						else if (old_state == STATE_CONDITION) {
-							// after a condition arg, there should be a function 
-							// but there might also be a combined condition
-							// either way, we are going to assume there will be a function
-							old_state = state;
-							state = STATE_FUNCTION;
-						} else {
-							set_error("UNKNOWNUSEOFCONDITIONARGUMENT");
-							goto force_error;
-						}
-					} else {
-						set_error("UNKNOWNSTATE");
-						goto force_error;
-					}
-
-					if (reject == false) {
-						if (!objectListAppend(obj)) {
-							objectListResize(object_list_size + 5);
-							if (!objectListAppend(obj)) {
-								// there should be no possible way it will reach this but 
-								// just in case we run out of memory and/or malloc failed
-								set_error("FAILEDTOAPPENDOBJECT");
-								// free the word_copy, we are aborting the function
-								sfree(word_copy);
-								goto force_error;
-							}
-						}
-					} else {
-						sfree(word_copy);
-					}
-					word_i = 0;
-				} else if (c == SPECIAL_COMBINE) {
-					// after [C] there is going to be [P]
-					if (state == STATE_PIN) {
-						// start adding the pins
-						old_state = state;
-						state = STATE_COMBINE;
-						dont_append = true;
-					} else if (state == STATE_PIN && old_state == STATE_COMBINE) {
-						// end
-						old_state = state;
-						state = STATE_FUNCTION;
-						dont_append = true;
-					} else if (state == STATE_FUNCTION && old_state == STATE_CONDITION_ARGUMENT) {
-						// multiple conditions, [pin] : < 500 @ > 0 :
-						// after a condition argument it is assumened to have a function
-						// but in this case not
-						old_state = state;
-						state = STATE_CONDITION;
-						dont_append = true;
-					} else {
-						set_error("INVALIDUSEOFCOMBINE");
-						goto force_error;
-					}
-				} else if (c == SPECIAL_CONDITION) {
-					// there is always pin before a condition
-					// but after a pin state becomes function
-					// so we check for the last state
-					if (state == STATE_FUNCTION && old_state == STATE_PIN) {
-						old_state = state;
-						state = STATE_CONDITION;
-						dont_append = true;
-					} else if (state == STATE_PIN && old_state == STATE_COMBINE) {
-						// combine just started therefore current state is pin
-						// multiple is also true
-						old_state = state;
-						state = STATE_CONDITION;
-						dont_append = true;
-					} else if (state == STATE_FUNCTION && old_state == STATE_CONDITION_ARGUMENT) {
-						// condition ended
-						old_state = state;
-						state = STATE_FUNCTION;
-						dont_append = true;
-					} else {
-						set_error("INVALIDUSEOFCONDITION");
-						goto force_error;
-					}
-				} else if (c == SPECIAL_END) {
-					if (state == STATE_ARGUMENT) {
-						// add the argument then reset the state
-						dont_append = true;
-						end = true;
-						goto force_object;
-					} else if (state == STATE_PIN && old_state == STATE_COMBINE) {
-						// add the argument then continue for the others
-						dont_append = true;
-						goto force_object;
-					} else if (state == STATE_FUNCTION) {
-						// no arguments for the function
-						// add the function and reset the state
-						dont_append = true;
-						end = true;
-						// we do this so it is not going to report an error after '\n' in the source code
-						old_state = state;
-						state = STATE_ARGUMENT;
-						goto force_object;
-					} else {
-						// probably just written ';' otherwise there is no way to get here
-						set_error("UNKOWNENDING");
-						goto force_error;
-					}
-				}
-
-				if (!dont_append) {
-					word[word_i] = c;
-					word_i++;
-				}
-			}
+		
 		}
 		i++;
 	}
